a187b1de3d74a2377cc367994091d2a61708a765
Janos Ujvari <janos.ujvari@hilandco.com>
2015-06-24 14:31:15 +0200

    test

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -29,7 +29,6 @@ detect_memory() {
 
 export PATH="$HOME/.heroku/node/bin:$HOME/bin:$HOME/node_modules/.bin:$PATH"
 export NODE_HOME="$HOME/.heroku/node"
-export PKG_CONFIG_PATH="$HOME/vendor/opencv/lib/pkgconfig"
 
 calculate_concurrency
 log_concurrency
